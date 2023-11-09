d8b9203ad572dec998de80fa68a33da69f1fe7a2
Adeel Ur Rehman <adeelharry@gmail.com>
2023-11-09 23:05:18 +0500

    Updated padding in FeatureSection.tsx

--- src/components/Feature/FeatureSection.tsx
+++ src/components/Feature/FeatureSection.tsx
@@ -22,24 +22,21 @@ const FeatureSection = ({
     <>
       <Container
         id="Projects"
-        // border={"1px"}
         minHeight="100vh"
         maxW={"container.xl"}
         centerContent
         alignItems="center"
         justifyContent="center"
-        px={{ base: 5, md: 4 }}
-        py={4}
+        px={{ base: 7, md: 4 }}
+        pt={{ base: 16, lg: 14 }}
+        py={{ base: 5, md: 4 }}
       >
         <Flex
-          // border={"1px"}
           minW={"100%"}
           display={{ base: "block", sm: "flex", md: "flex", lg: "flex" }}
           flexDirection={{ base: "column", lg: "row" }}
           alignItems={"center"}
           justifyContent={"space-between"}
-          // columns={{ base: 1, lg: 2 }}
-          // spacing={{ base: 7, md: 10, lg: 40 }}
         >
           <TitleCard imageSrc={imgSrc}>{cardTitle}</TitleCard>
 
@@ -47,7 +44,6 @@ const FeatureSection = ({
             pt={5}
             textAlign={{ base: "center", lg: "start" }}
             pr={{ lg: 5 }}
-            // border={"1px"}
           >
             <FeatureContent heading={contentHeading} applyMargin={true}>
               {children}
